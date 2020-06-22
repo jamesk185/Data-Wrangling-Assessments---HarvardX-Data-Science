4a9e09b5f12d44407610cfd085a6107526a6ab80
James Kowalik <jameskowalik@outlook.com>
2020-06-22 22:50:52 +0900

    2.3 web scraping complete

--- code.R
+++ code.R
@@ -17,3 +17,34 @@ awards_players_2016 <- AwardsPlayers %>% filter(yearID == 2016) %>%
 top_names_IDonly <- top_names %>% select(playerID)
 intersect(top_names_IDonly, awards_players_2016)
 setdiff(awards_players_2016, top_names_IDonly)
+
+
+# 2.3 Web Scraping
+library(rvest)
+url <- "https://web.archive.org/web/20181024132313/http://www.stevetheump.com/Payrolls.htm"
+h <- read_html(url)
+nodes <- html_nodes(h, "table")
+html_text(nodes[[8]])
+html_table(nodes[[8]])
+html_table(nodes[[1]])
+html_table(nodes[[2]])
+html_table(nodes[[3]])
+html_table(nodes[[4]])
+html_table(nodes[[19]])
+html_table(nodes[[20]])
+html_table(nodes[[21]])
+tab_1 <- html_table(nodes[[10]]) %>% select(-X1) %>% .[-1,] %>% setNames(c("Team", "Payroll", "Average"))
+tab_2 <- html_table(nodes[[19]]) %>% .[-1,] %>% setNames(c("Team", "Payroll", "Average"))
+full_join(tab_1, tab_2, by="Team")
+
+library(rvest)
+library(tidyverse)
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+i <- read_html(url)
+tab <- html_nodes(i, "table")
+tab
+html_table(tab[[1]], fill=TRUE)
+html_table(tab[[2]], fill=TRUE)
+html_table(tab[[3]], fill=TRUE)
+html_table(tab[[4]], fill=TRUE)
+head(html_table(tab[[5]], fill=TRUE))
