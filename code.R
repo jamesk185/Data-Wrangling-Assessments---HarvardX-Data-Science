fd390f93476e335f5ec097087b72305ed0ab7f09
James Kowalik <jameskowalik@outlook.com>
2020-06-22 20:10:02 +0900

    First upload for proj, with preliminary code for questions 5-7.

---
+++ code.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(Lahman)
+top <- Batting %>% 
+  filter(yearID == 2016) %>%
+  arrange(desc(HR)) %>%    # arrange by descending HR count
+  slice(1:10)    # take entries 1-10
+top %>% as_tibble()
+Master %>% as_tibble()
